9826f53b5cf3c94d455592abd6b9308146fe1f02
Harshith Doppalapudi <86398429+D-Harshith@users.noreply.github.com>
2025-05-28 21:46:55 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { WebSocketServer, WebSocket } from "ws";
+import { WebSocketServer} from "ws";
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
